9accd97d3b5a0cf6a7fad27181f274b257696ff0
hanniel.daniel <hanniel.daniel@cinfores.com>
2025-07-31 14:02:33 +0100

    added amdin login feature

--- src/Screens/auth/Login.tsx
+++ src/Screens/auth/Login.tsx
@@ -4,10 +4,65 @@ import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import { useNavigate } from 'react-router-dom'
+import axios from "axios"
+import { log } from 'console'
+import { Toast } from '@/components/Toast'
 
 const Login = () => {
 
   const navigate = useNavigate();
+
+  const handleAdminLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+
+    try {
+      const response = await axios.post("http://localhost:8000/api/login", {
+        email,
+        password,
+      }, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        // withCredentials: true, // uncomment if using cookies
+      });
+
+      console.log("Login successful:", response.data);
+      Toast.fire({
+        icon: 'success',
+        title: response.data.message,
+      });
+
+
+      // Optionally store token or user info
+      // localStorage.setItem("token", response.data.token);
+
+      // Redirect to dashboard
+      navigate("/dashboard");
+    } catch (error: any) {
+      if (error.response) {
+        console.error("Login failed:", error.response.data);
+        Toast.fire({
+          icon: 'error',
+          title: error.response.data.error,
+        })
+      } else {
+        console.error("Network error:", error.message);
+        Toast.fire({
+          icon: 'error',
+          title: "Network error",
+        })
+      }
+    }
+  };
+
+
+  const handleStudentLogin = () => {
+
+  }
 
   return (
     <div className="flex items-center justify-center min-h-screen px-4">
@@ -31,7 +86,7 @@ const Login = () => {
               </div>
             </CardContent>
             <CardFooter className='flex justify-end'>
-              <Button onClick={()=> navigate('/user')}>Login</Button>
+              <Button onClick={() => navigate('/user')}>Login</Button>
             </CardFooter>
           </Card>
         </TabsContent>
@@ -39,24 +94,26 @@ const Login = () => {
         {/* Admin */}
         <TabsContent value="admin">
           <Card>
-            <CardHeader>
-              <CardTitle>Admin Login</CardTitle>
-              <CardDescription>Enter your admin credentials</CardDescription>
-            </CardHeader>
-            <CardContent className="grid gap-6">
-              <div className="grid gap-3">
-                <Label htmlFor="username">Username</Label>
-                <Input id="username" type="text" placeholder='John.Doe' />
-              </div>
-              <div className="grid gap-3">
-                <Label htmlFor="password">Password</Label>
-                <Input id="password" type="password" placeholder='Enter Password' />
-              </div>
-            </CardContent>
-            <CardFooter className='flex justify-between'>
-              <a href="/forgot-password" className="text-sm underline-offset-4 hover:underline">Forgot your password?</a>
-              <Button onClick={()=> navigate('/dashboard')}>Login</Button>
-            </CardFooter>
+            <form onSubmit={handleAdminLogin}>
+              <CardHeader>
+                <CardTitle>Admin Login</CardTitle>
+                <CardDescription>Enter your admin credentials</CardDescription>
+              </CardHeader>
+              <CardContent className="grid gap-6">
+                <div className="grid gap-3">
+                  <Label htmlFor="email">Email</Label>
+                  <Input id="email" type="text" name="email" placeholder='john@example.com' />
+                </div>
+                <div className="grid gap-3">
+                  <Label htmlFor="password">Password</Label>
+                  <Input id="password" type="password" name="password" placeholder='Enter Password' />
+                </div>
+              </CardContent>
+              <CardFooter className='flex justify-between'>
+                <a href="/forgot-password" className="text-sm underline-offset-4 hover:underline">Forgot your password?</a>
+                <Button type='submit'>Login</Button>
+              </CardFooter>
+            </form>
           </Card>
         </TabsContent>
       </Tabs>
--- src/Screens/user/User.tsx
+++ src/Screens/user/User.tsx
@@ -1,7 +1,24 @@
+import { Button } from "@/components/ui/button"
+import { useNavigate } from "react-router-dom"
 
 const User = () => {
+  const navigate = useNavigate();
+
   return (
-    <div>User</div>
+    <main className="h-screen w-full flex flex-col items-center justify-center">
+      <section className="p-5 rounded-xl bg-muted md:min-w-xl flex items-center gap-4">
+        <div className="bg-background w-[80px] h-[80px] md:w-[120px] md:h-[100px] flex items-center justify-center rounded-xl font-bold uppercase text-3xl text-center">
+          <h1>hi</h1>
+        </div>
+        <div className="w-full">
+          <h2 className="font-semibold md:text-2xl">Hanniel Daniel</h2>
+          <p className="text-xs">De.2024/PT/0600</p>
+          <div className="flex justify-end w-full mt-2">
+            <Button className="font-bold" onClick={()=> navigate('/')}>Appraise</Button>
+          </div>
+        </div>
+      </section>
+    </main>
   )
 }
 
